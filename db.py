d36c0c12c08666382322c0e951831b6de1fb7b8c
Arjun <cosmicpepe2001@gmail.com>
2021-03-15 21:19:50 +0530

    initial commit

--- db.py
+++ db.py
@@ -1,21 +1,12 @@
 from flask import Flask
 import pymongo
 
-#from app import app
 app=Flask(__name__)
 
-#CONNECTION_STRING = "mongodb+srv://in:in@cluster0.p9jmu.mongodb.net/geny?retryWrites=true&w=majority"
 CONNECTION_STRING = "mongodb+srv://swarnabha:swarnabhaflask@cluster0.sbgtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-#CONNECTION_STRING = "mongodb+srv://hellocp:flaskapi2@cluster0.ynvts.mongodb.net/sentimentApi?retryWrites=true&w=majority"
-#CONNECTION_STRING = "mongodb://localhost:27017/sentimental2"
+
 client = pymongo.MongoClient(CONNECTION_STRING)
-#db = client['sentimentApi']
-# user_collection = pymongo.collection.Collection(db, 'helloCollection')
-#user_collection = db['sentiment Analysis']
+
 db = client.get_database('sentimentApi')
 user_collection = pymongo.collection.Collection(db, 'sentimentAnalysis')
 
-# CONNECTION_STRING = "mongodb+srv://test:test@flask-mongodb-atlas-1g8po.mongodb.net/test?retryWrites=true&w=majority"
-# client = pymongo.MongoClient(CONNECTION_STRING)
-# db = client.get_database('flask_mongodb_atlas')
-# user_collection = pymongo.collection.Collection(db, 'user_collection')
--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ import nltk
 import json
 import pymongo
 from flask_cors import CORS, cross_origin
-#nltk.download("punkt")
+nltk.download("punkt")
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -16,82 +16,8 @@ import db
 from db import *
 api = Api(app)
 
-# def parse_json(data):
-#     return json.loads(dumps(data))
-
-puppies = []
-risk_words = {
-    'not good' : 'bad',
-    'not bad' : 'good',
-    'not nice' : 'bad',
-    'hate' : 'bad'
-}
-def find_features(document):
-        with open("word_features_final.txt", "rb") as fp:
-            word_features = pickle.load(fp)
-        document = document.lower()
-        words = word_tokenize(document)
-        document_words = set(words)
-        features = {}
-        for w in word_features:
-           features['contains({})'.format(w)] = (w in document_words)
-        for w in risk_words.keys():
-         if w in document:
-          features['contains({})'.format(risk_words[w])] = (w in document)
-          if w != 'hate':
-            features['contains({})'.format(w.split()[1])] = False
-        return features
-
-# with open("word_features_final.txt", "rb") as fp:
-#             word_features = pickle.load(fp)
-# class Sentiment(Resource):
-    
-#     def get(self,text):  
-#         print(text)
-#         loaded_model = pickle.load(open("SentiAnalysis.sav", 'rb'))
-
-#         feats = find_features(text)
-        
-#         result = loaded_model.classify(feats)
-#         print(result)
-#         return result
-                    
 
 
-# api.add_resource(Sentiment, '/sentiment/<string:text>')
-@app.route("/",methods=['GET'])
-@cross_origin()
-def get1():
-    return("flask server is up and running")
-
-
-
-@app.route("/test/<string:text>",methods=['POST'])
-@cross_origin()
-def get(text):  
-        print(text)
-        loaded_model = pickle.load(open("SentiAnalysisfinal.sav", 'rb'))
-        feats = find_features(text)        
-        result = loaded_model.classify(feats)
-        user_collection.insert_one({"status": result})
-        print(result)
-        return jsonify({'ModelResponse': result})
-    
-
-@app.route("/test2",methods=['GET'])
-@cross_origin()
-def getty():
-    positive = user_collection.count_documents({"status":"positive"})    
-    negative = user_collection.count_documents({"status":"negative"})
-    print(positive)
-    print(negative)
-    res = {}
-    res['Positive'] = positive
-    res['Negative'] = negative    
-    return jsonify(res)
-    # db.db.collection.insert_one({"name": "John"})
-    # return "Connected to the data base!"
-    
     
 
 if __name__ == '__main__':
